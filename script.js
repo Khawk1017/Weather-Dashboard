b555a67dfd225d580e13a182fe1e9cbf372ac4fa
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-02-07 15:58:51 -0700

    updated index and script files

--- script.js
+++ script.js
@@ -2,16 +2,28 @@
 
 const submitBtn = document.querySelector('button');
 const weather_box = document.querySelector('.weather');
-const cityInput = document.querySelector('.city');
-const city_temp = document.querySelector('.temperature');
+const city = document.getElementById('city');
+const city_temp = document.getElementById('temperature');
 const cards = document.querySelector('allCards');
 
-submitBtn.addEventListener('click', function() {
-  const city = cityInput.value;
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=036d5581dff1701982052a13de844a4f`)
+fetch("https://api.openweathermap.org/data/2.5/weather?q=London&appid=036d5581dff1701982052a13de844a4f")
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-      // Use the data to update the weather information
+        console.log(data);
+
     });
-});
+document.getElementById('thisBtn').addEventListener('click', function (event) {
+    event.preventDefault()
+    var city_name = document.getElementById('city_input').value
+    console.log("City", city_name)
+    getForcasts(city_name)
+})
+
+function getForcasts(city_name) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=036d5581dff1701982052a13de844a4f`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data, "api");
+
+        });
+}
